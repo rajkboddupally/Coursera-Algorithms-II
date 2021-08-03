629cad78d5cfb5bf4c0390d7d05b5e9e6edb4d31
Raj Kumar Boddupally <rajkboddupally@gmaill.com>
2021-08-03 16:38:30 -0400

    Initial commit

---
+++ src/main/java/com/Graph.java
@@ -0,0 +1,27 @@
+package main.java.com;
+
+/* Raj Kumar Boddupally created on 8/3/2021 inside the package - main.java */
+
+import edu.princeton.cs.algs4.Bag;
+
+public class Graph {
+    private final int V;
+    private Bag<Integer>[] adj;
+
+    public Graph(int v) {
+        this.V = v;
+        this.adj = (Bag<Integer>[]) new Bag[v];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new Bag<Integer>();
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+}
---
+++ src/test/java/com/GraphApiTest.java
@@ -0,0 +1,27 @@
+package test.java.com;
+
+/* Raj Kumar Boddupally created on 8/3/2021 inside the package - test.java.com */
+
+import main.java.com.Graph;
+
+import java.util.stream.StreamSupport;
+
+public class GraphApiTest {
+    @Test
+    public void createGraph() {
+        Graph graph = new Graph(5);
+        graph.addEdge(0, 4);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+
+        long count2 = StreamSupport.stream(graph.adj(2).spliterator(), false).count();
+
+        Assertions.assertEquals(2, count2);
+
+        //Assert.assertEquals(3, StreamSupport.stream(graph.adj(4).spliterator(), false).count());
+    }
+
+
+}
