93521dac7f8bf0cb7be1e3f6716745c395564ced
Raj Kumar Boddupally <rajkboddupally@gmaill.com>
2021-08-25 07:55:42 -0400

    DiameterAndCenter and BFS

--- src/main/java/main/java/com/week1/DiameterAndCenter.java
+++ src/main/java/main/java/com/week1/DiameterAndCenter.java
@@ -14,10 +14,55 @@ Center: design a linear-time algorithm to find a vertex such that its maximum di
 
  */
 public class DiameterAndCenter {
-    private final int diameter;
     private int center;
+    private final Graph graph;
+    BreadthFirstPaths bfs1;
 
     public DiameterAndCenter(Graph graph) {
+        this.graph = graph;
+
+
+    }
+
+    public static void main(String[] args) {
+        Graph g1 = new Graph(7);
+        g1.addEdge(1, 0);
+        g1.addEdge(0, 2);
+        g1.addEdge(0, 3);
+        g1.addEdge(3, 4);
+        g1.addEdge(2, 6);
+        DiameterAndCenter d = new DiameterAndCenter(g1);
+        for (Integer w : d.getDiameter()) {
+            System.out.print(w + " ");
+        }
+        System.out.println();
+
+        Graph g2 = new Graph(15);
+        g2.addEdge(1, 3);
+        g2.addEdge(12, 3);
+        g2.addEdge(12, 9);
+        g2.addEdge(12, 8);
+        g2.addEdge(6, 8);
+        g2.addEdge(4, 8);
+        g2.addEdge(2, 8);
+        g2.addEdge(8, 7);
+        g2.addEdge(7, 11);
+        g2.addEdge(10, 7);
+        g2.addEdge(13, 11);
+        g2.addEdge(11, 14);
+        g2.addEdge(5, 11);
+        g2.addEdge(0, 11);
+        DiameterAndCenter d2 = new DiameterAndCenter(g2);
+        for (Integer w : d2.getDiameter()) {
+            System.out.print(w + " ");
+        }
+    }
+
+    public int getCenter() {
+        return this.center;
+    }
+
+    public Iterable<Integer> getDiameter() {
         int w;
         int v = 0;
         for (w = 0; w < graph.V(); w++) {
@@ -35,31 +80,14 @@ public class DiameterAndCenter {
                 v = w;
             }
         }
-        BreadthFirstPaths bfs1 = new BreadthFirstPaths(graph, v);
+        bfs1 = new BreadthFirstPaths(graph, v);
         for (w = 0; w < graph.V(); w++) {
             farthestDistance = Math.max(farthestDistance, bfs1.distanceTo(w));
+            if (farthestDistance < bfs1.distanceTo(w)) {
+                farthestDistance = bfs1.distanceTo(w);
+                farthestVertex = w;
+            }
         }
-        this.diameter = farthestDistance;
-
-    }
-
-
-    public int getDiameter() {
-        return this.diameter;
-    }
-
-    public int getCenter() {
-        return this.center;
-    }
-
-    public static void main(String[] args) {
-        Graph g1 = new Graph(7);
-        g1.addEdge(1, 0);
-        g1.addEdge(0, 2);
-        g1.addEdge(0, 3);
-        g1.addEdge(3, 4);
-        g1.addEdge(2, 6);
-        DiameterAndCenter d = new DiameterAndCenter(g1);
-        System.out.println(d.getDiameter());
+        return bfs1.path(farthestVertex);
     }
 }
